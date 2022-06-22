6acd92b08b68f92933db3f65e3c08444d7630002
glayson <glayson.visgueira@paraiba.email>
2022-06-22 17:33:38 -0300

    change font style

--- styles/globalStyle.jsx
+++ styles/globalStyle.jsx
@@ -2,6 +2,9 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle` 
 @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;0,800;1,800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Rubik+Mono+One&display=swap');
 * {
   margin: 0;
    padding: 0;
@@ -10,8 +13,7 @@ const GlobalStyle = createGlobalStyle`
   }
   
   html, body, :root {
-    font-family: "Quicksand", sans-serif;
-    font-weight: 400;
+    font-family: 'Rubik Mono One', sans-serif;
     min-height: 100%;    
     scroll-behavior: smooth;
   }  
@@ -42,3 +44,8 @@ const GlobalStyle = createGlobalStyle`
   
 `;
 export default GlobalStyle;
+
+//font-family: "Quicksand", sans-serif;
+//font-family: 'Open Sans', sans-serif;
+//font-family: 'Rubik Mono One', sans-serif;
+//font-family: 'Roboto', sans-serif;

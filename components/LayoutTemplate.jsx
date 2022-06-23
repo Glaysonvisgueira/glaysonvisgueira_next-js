c9baa709a0d394bebbb52fd07828580626aa1a4e
glayson <glayson.visgueira@paraiba.email>
2022-06-23 17:53:04 -0300

    add import

--- components/LayoutTemplate.jsx
+++ components/LayoutTemplate.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import styled from "styled-components";
+import ScrollAnimation from "react-animate-on-scroll";
 
 //Custom components
 import ThemeButtons from "@/components/ThemeButtons";

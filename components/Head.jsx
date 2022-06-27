cdcf2494216158310eba7afaf738b44b81fc435b
glayson <glayson.visgueira@paraiba.email>
2022-06-27 09:20:15 -0300

    just some identation adjust

--- components/Head.jsx
+++ components/Head.jsx
@@ -6,12 +6,9 @@ export default function HeadTag(props) {
 	return (
 		<Head>
 			<title>{props.title}</title>
-			{/* <!-- Chrome, Firefox OS and Opera -->*/}
-			<meta name="theme-color" content={theme.colors.branding} />
-			{/* <!-- Windows Phone -->*/}
-			<meta name="msapplication-navbutton-color" content={theme.colors.branding} />
-			{/*<!-- iOS Safari -->*/}
-			<meta name="apple-mobile-web-app-status-bar-style" content={theme.colors.branding} />
+			<meta name="theme-color" content={theme.colors.branding} /> {/* <!-- Chrome, Firefox OS and Opera -->*/}
+			<meta name="msapplication-navbutton-color" content={theme.colors.branding} /> {/* <!-- Windows Phone -->*/}
+			<meta name="apple-mobile-web-app-status-bar-style" content={theme.colors.branding} /> {/*<!-- iOS Safari -->*/}
 			<meta name="description" content={props.metaDescription} />
 			<meta name="keywords" content={props.keywords} />
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></link>

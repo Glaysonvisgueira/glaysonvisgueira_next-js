1794173686c0b137a052d2d8af74a06cf173b22d
glayson <glayson.visgueira@paraiba.email>
2022-11-17 09:49:45 -0300

    Change favicon.ico

--- components/Head.jsx
+++ components/Head.jsx
@@ -13,6 +13,10 @@ export default function HeadTag(props) {
 			<meta name="description" content={props.metaDescription} />
 			<meta name="keywords" content={props.keywords} />
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></link>
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+			<link rel="manifest" href="/site.webmanifest"></link>
 		</Head>
 	);
 }

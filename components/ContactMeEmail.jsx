022305b27a2b57979332a11dcbee1a2155026da0
glayson <glayson.visgueira@paraiba.email>
2023-03-24 15:52:18 -0300

    Update contact email field

--- components/ContactMeEmail.jsx
+++ components/ContactMeEmail.jsx
@@ -20,6 +20,14 @@ const ContactMeContainer = styled.div`
 	height: 400px;
 	padding: 20px;
 	background-color: ${(props) => props.theme.colors.branding};
+	/* background-color: ${(props) => props.theme.colors.backgroundSecondary}; */
+`;
+
+const ContainerForm = styled.form`
+	width: 40%;
+	padding: 20px;
+	border-radius: 8px;
+	border: 1px solid ${(props) => props.theme.colors.background};
 `;
 
 const WrapperInput = styled.div`
@@ -34,17 +42,36 @@ const WrapperInput = styled.div`
 		color: ${(props) => props.theme.colors.background};
 		font-size: 1rem;
 		margin-bottom: 5px;
+		margin-top: 10px;
+		font-weight: bold;
 	}
 
 	input {
 		border: none;
-		width: 300px;
+		width: 100%;
 		height: 34px;
 		border-radius: 4px;
 		padding: 0 10px;
+		border: 2px solid transparent;
+		caret-color: ${(props) => props.theme.colors.branding};
 	}
 
 	input:focus {
+		border: 2px solid ${(props) => props.theme.colors.background};
+	}
+
+	textarea {
+		border: 2px solid transparent;
+		width: 100%;
+		max-width: 100%;
+		min-height: 80px;
+		border-radius: 4px;
+		max-height: 120px;
+		padding: 10px;
+		/* resize: vertical; */
+	}
+
+	textarea:focus {
 		border: 2px solid ${(props) => props.theme.colors.background};
 	}
 `;
@@ -54,18 +81,22 @@ export default function ContactMeEmail() {
 
 	return (
 		<ContactMeContainer>
-			<WrapperInput>
-				<label>Nome</label>
-				<input />
-			</WrapperInput>
-			<WrapperInput>
-				<label>Nome</label>
-				<input />
-			</WrapperInput>
-			<WrapperInput>
-				<label>Nome</label>
-				<input />
-			</WrapperInput>
+			<ContainerForm>
+				<WrapperInput>
+					<label for="nome">Nome</label>
+					<input type="text" placeholder="Seu nome" id="nome" name="nome" />
+				</WrapperInput>
+
+				<WrapperInput>
+					<label>E-mail</label>
+					<input type="text" placeholder="seuemail@email.com" />
+				</WrapperInput>
+
+				<WrapperInput>
+					<label>Mensagem</label>
+					<textarea placeholder="Sua mensagem..." />
+				</WrapperInput>
+			</ContainerForm>
 		</ContactMeContainer>
 	);
 }
